505b2eaa5bb516665b82a1e9c74b204e447ed6e8
YuGoncharova <63481160+YuGoncharova@users.noreply.github.com>
2020-04-18 22:17:13 +0100

    Melhoramento da estrutura do código

--- CU-Sensebox/Raspberry/pi_code.py
+++ CU-Sensebox/Raspberry/pi_code.py
@@ -4,7 +4,7 @@ from raspi_lora import LoRa, ModemConfig
 def on_recv(payload):
     print("Received:", payload.message)
 
-   #The selected SPI channel is 0, The selected interrupt pin is GPIO17
+#The selected SPI channel is 0, The selected interrupt pin is GPIO17
 lora = LoRa(0, 17, 2, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
 
 lora.on_recv = on_recv

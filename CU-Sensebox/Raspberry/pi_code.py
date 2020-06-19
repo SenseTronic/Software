15555015ca7eb76571c44f4dce797609fcab03aa
YuGoncharova <63481160+YuGoncharova@users.noreply.github.com>
2020-06-19 01:15:32 +0100

    Final update

--- CU-Sensebox/Raspberry/pi_code.py
+++ CU-Sensebox/Raspberry/pi_code.py
@@ -1,16 +1,22 @@
 from raspi_lora import LoRa, ModemConfig
+import time
 
 # This is our callback function that runs when a message is received
 def on_recv(payload):
+    print("From:", payload.header_from)
     print("Received:", payload.message)
-
-#The selected SPI channel is 0, The selected interrupt pin is GPIO17
-lora = LoRa(0, 17, 2, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
-
+    
+# This is a configuration function
+# The selected SPI channel is 0, The selected interrupt pin is GPIO17
+lora = LoRa(0, 17, 2, freq=868, acks = False)
 lora.on_recv = on_recv
 
-while True:
-    print("1")
+# Sets the radio to rx(receive) mode
+lora.set_mode_rx()
 
-# And remember to call this as your program exits...
-lora.close()
+try:
+    while True:
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print('Stopping...')
+    lora.close()

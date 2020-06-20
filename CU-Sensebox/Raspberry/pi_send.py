2bef438abf400ed19a6da9b3e7a6620c5f3bd4d9
YuGoncharova <63481160+YuGoncharova@users.noreply.github.com>
2020-06-20 04:00:05 +0100

    Update pi_send.py

--- CU-Sensebox/Raspberry/pi_send.py
+++ CU-Sensebox/Raspberry/pi_send.py
@@ -1,19 +1,22 @@
 from raspi_lora import LoRa, ModemConfig
 import time
 
-# This is our callback function that runs when a message is received
 def on_recv(payload):
     print("From:", payload.header_from)
     print("Received:", payload.message)
 
-#The selected SPI channel is 0, The selected interrupt pin is GPIO17
-lora = LoRa(0, 17, 2, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=False)
-
+lora = LoRa(0, 17, 2, freq=868, acks = False)
 lora.on_recv = on_recv
 
-while True:
-    send_to_wait("teste", 255, header_flags=0)
-    time.sleep(2)
+lora.set_mode_tx()
 
-# And remember to call this as your program exits... 
-lora.close()
+message = "Ping"
+
+try:
+    while True:
+        lora.send(message, 2, 255, header_flags=0)
+        print('message sent')
+        time.sleep(1)
+except KeyboardInterrupt:
+    print('Stopping...')
+    lora.close()
